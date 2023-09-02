1e41e134c2c7f38548748c84b4550a81bf00b120
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-09-02 23:44:02 +0100

    add ngx toastr

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
@@ -5,10 +6,17 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { AppRoutes } from './app/app-routes';
 import { HttpClientModule } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
+import { provideToastr } from 'ngx-toastr';
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule, HttpClientModule, CookieService),
     provideRouter(AppRoutes, withComponentInputBinding()),
+    provideAnimations(), // required animations providers
+    provideToastr({
+      timeOut: 6000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    }),
   ],
 }).catch(console.error);
