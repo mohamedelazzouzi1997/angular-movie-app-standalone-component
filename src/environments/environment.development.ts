34df30ae1a03083d5be61741a7bde5cecb7df08b
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-09-03 00:05:12 +0100

    add git ignote to environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  domain: 'http://localhost:4200/',
+  domain: 'https://angular-16-standalone-movie-7jiw0ka2t-mohamedelazzouzi1997.vercel.app/',
   apiKey: 'b77fafd8a5792126db36ead00b5ce42f',
   baseUrl: 'https://api.themoviedb.org/3',
   isProd: false
