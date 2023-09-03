4e374735eaf93f206f41b35b6d401e2ec4761ad9
mohamed el azzouzi <med.elazzouzi.97@gmail.com>
2023-09-03 14:40:42 +0100

    vercel link

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  // domain: 'https://mohamed-elazzouzi-angular-movie-app.vercel.app/',
-  domain: 'http://localhost:4200/',
+  domain: 'https://mohamed-elazzouzi-angular-movie-app.vercel.app/',
+  // domain: 'http://localhost:4200/',
   apiKey: 'b77fafd8a5792126db36ead00b5ce42f',
   baseUrl: 'https://api.themoviedb.org/3',
   isProd: false
